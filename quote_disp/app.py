9d263bc85a3db28868a19b6a7585dc381e95d442
jjpal <36558688+jjpal@users.noreply.github.com>
2023-07-01 18:19:18 -0500

    Update app.py ex8 opt p1

--- quote_disp/app.py
+++ quote_disp/app.py
@@ -18,10 +18,18 @@ def home():
 
 @app.route("/get_quote")
 def quote():
-    quote = requests.get("http://quote-gen-container:5000/quote").text
-    print("quote - ", quote)
-
-    return render_template("quote.html", quote=quote)
+hosts = [
+         'http://week2-devops-web1-1:5000/quote',
+         'http://week2-devops-web1-2:5000/quote1'
+     ]
+quote = "Quote service is unavailable"
+for host in hosts:
+    r = requests.get(host)
+    if r.status_code == 200:
+        quote = r.text
+        break
+        
+return render_template("quote.html", quote=quote)
 
 
 if __name__ == "__main__":
